ea70390b690b97cb40703ed6709f7f0384fcb1f7
moatazgamal100 <moatazfahmy100100@gmail.com>
2024-03-20 01:20:40 +0200

    Finish Edit Animation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+ 
   $(".owl-carousel").owlCarousel({
     items: 4,
     autoplay: true,
@@ -12,22 +13,55 @@ $(document).ready(function () {
     dotsData: true,
   });
   $(".owl-dot").text("");
-  $(window).scroll(function () {
-    let windowscroll = $(Window).scrollTop();
+  
+  
+});
 
-    
-    if (windowscroll >= 600) {
-      console.log("hello");
-      $(".img-front").css('animation' , 'img 2s forwards');
-      $(".img-back").css('animation' , 'imgg 2s forwards');
-      $(".side-div").css('animation' , 'side 2s forwards');
-      $(".right-side").css('animation' , 'rightside 2s forwards');
+$(window).scroll(function () {
+  
+});
+
+
+let lastScrollTop = 0;
+
+$(window).scroll(function() {
+    let currentScroll = $(this).scrollTop();
+
+    let windowscroll = $(Window).scrollTop();
+    if (currentScroll > lastScrollTop) {
+
+      if (windowscroll >= 600) {
+        $(".img-front").css('animation' , 'img-finish 2s forwards');
+        $(".img-back").css('animation' , 'imgg-finish 2s forwards');
+        $(".right-side").css('animation' , 'rightside-finish 2s forwards');
+        $(".side-div").css('animation' , 'side-finish 2s forwards');
+      }
+      else if (windowscroll >= 400) {
+        $(".img-front").css('animation' , 'img 2s forwards');
+        $(".img-back").css('animation' , 'imgg 2s forwards');
+        $(".right-side").css('animation' , 'rightside 2s forwards');
+        $(".side-div").css('animation' , 'side 2s forwards');
+      }
+      
+      console.log("Scrolling down");
+    } else {
+      // User is scrolling up
+      if (windowscroll <= 400) {
+        $(".img-front").css('animation' , 'imgback 2s forwards');
+        $(".img-back").css('animation' , 'imgg-back 2s forwards');
+        $(".side-div").css('animation' , 'side-back 2s forwards');
+        $(".right-side").css('animation' , 'rightside-back 2s forwards');
+        
+      }
+      else if (windowscroll <= 600) {
+        $(".img-front").css('animation', 'img-finishback 2s forwards');
+        $(".img-back").css('animation' , 'imggfinish-back 2s forwards');
+        $(".side-div").css('animation' , 'side-finishback 2s forwards');
+        $(".right-side").css('animation' , 'rightside-finishback 2s forwards');
+      }
+      
+        console.log("Scrolling up");
     }
-    else{
-      $(".img-front").css('animation' , 'img-back 2s forwards');
-      $(".img-back").css('animation' , 'imgg-back 2s forwards');
-      $(".side-div").css('animation' , 'side-back 2s forwards');
-      $(".right-side").css('animation' , 'rightside-back 2s forwards');
-    }
-  });
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
 });
